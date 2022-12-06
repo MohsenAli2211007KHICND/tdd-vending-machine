23b391541de6d2f943a09fadd7be2cbb239ed79c
MohsenAli2211007KHICND <mohsen.ali@xloopdigital.com>
2022-12-06 17:05:27 +0500

    AC:1 should have items to purchase success

--- src/Machine.js
+++ src/Machine.js
@@ -1,9 +1,11 @@
+let items = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}]
 module.exports = class Machine {
+   
     constructor() {
+   
 
     }
-
-
     seeSelections() {
+        return items
     }
 };
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -5,12 +5,10 @@ describe('the vending machine', () => {
         // setup
         const machine = new Machine();
         const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
-
         // exercise
         const actual = machine.seeSelections();
-
         // assert
-        expect(true).toEqual(true);
+        expect(expected).toEqual(actual);
     });
 
 });
