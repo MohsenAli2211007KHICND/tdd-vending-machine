7981161cc722c65e0c542f99d2e5892b53472f89
MohsenAli2211007KHICND <mohsen.ali@xloopdigital.com>
2022-12-06 17:42:37 +0500

    AC: 3 should show my total deposited amount done

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -17,11 +17,12 @@ describe('the vending machine', () => {
     // `deposit(100)` returns `'You have deposited Rs 100'`
     // The machine should accept bills in these amounts: `10, 20, 50, 100, 500`
     it('should show money i have deposited', () => {
-        // setup
+       // setup
         const machine = new Machine();
-        let depositedMoney = machine.deposite(25)
+        let depositedMoney = machine.deposite(100)
         // exercise
         let expected
+        let actual = `You have deposited Rs ${depositedMoney}`
         if (depositedMoney===null){
              expected =null
         }
@@ -30,7 +31,23 @@ describe('the vending machine', () => {
         }
         
         // assert
-        expect(expected).toEqual(depositedMoney);
+        expect(expected).toEqual(actual);
     });
+   // As a customer, I want to add additional money, so that I can use the denominations I have to purchase an item.
+    //  Given I have deposited money in the vending machine,
+    //  when I deposit additional money,
+    //  then I see the new total on a screen. 
+    //  After depositing Rs 100, `deposit(50)` returns `'You have deposited Rs 150'`
+    it('should show my total deposited amount', () => {
+        // setup
+        const machine = new Machine();
+        let expected = 'You have deposited Rs 150'
+        let depositedMoney = machine.deposite(100)
+        depositedMoney += machine.deposite(50)
+        // exercise
+        let actual = `You have deposited Rs ${depositedMoney}`
+       // assert
+       expect(expected).toEqual(actual);
+        });
 
 });
